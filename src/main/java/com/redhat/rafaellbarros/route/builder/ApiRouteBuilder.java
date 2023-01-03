8940f3a0a126cdc9bb59115dba35539c488c19c6
Rafael Barros <rafaelbarros.softwareengineer@gmail.com>
2023-01-03 18:21:10 -0300

    :construction: Add dependency and create ApiRouteBuilder

---
+++ src/main/java/com/redhat/rafaellbarros/route/builder/ApiRouteBuilder.java
@@ -0,0 +1,11 @@
+package com.redhat.rafaellbarros.route.builder;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class ApiRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
